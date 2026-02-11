09c0695179291420dfb7306f1eb26915c1fd8f21
Udayreddy8339 <udaykumar@bhrish.com>
2026-02-11 13:15:14 +0530

    adding the lamebda function in app.js file

--- mui-react-sample/src/App.jsx
+++ mui-react-sample/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { Button, Box } from "@mui/material";
 import LoginDialog from "./components/LoginDialog";
 
 function App() {
   const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+
+  // 🔥 Lambda API Call
+  useEffect(() => {
+    fetch("https://lk8phf4iog.execute-api.ap-south-1.amazonaws.com/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
 
   return (
     <Box
       sx={{display: "flex",gap: 2, justifyContent: "center",mt: 5,              
       }}
     >
+      <Typography variant="h4">
+        React + Lambda Integration
+      </Typography>
+
+      <Typography variant="body1">
+        {message}
+      </Typography>
       <Button variant="contained" onClick={() => setOpen(true)}>
         Login Page
       </Button>
